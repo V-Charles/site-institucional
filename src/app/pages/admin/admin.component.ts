e26ea2f950a20cc04ff56d78676095977907d054
Vinicius Charles <viniciuscharles3101@gmail.com>
2026-05-27 10:05:45 -0300

    feat: cria interface da tabela do crud com listagem e exclusao

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ColaboradorService } from '../../services/colaborador.service';
+import { Colaborador } from '../../models/colaborador.model';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+  colaboradores: Colaborador[] = [];
+  colunasExibidas: string[] = ['id', 'nome', 'cargo', 'departamento', 'acoes'];
 
+  constructor(private colaboradorService: ColaboradorService) { }
+
+  ngOnInit(): void {
+    this.carregarColaboradores();
+  }
+
+  carregarColaboradores(): void {
+    this.colaboradorService.listar().subscribe(dados => {
+      this.colaboradores = dados;
+    });
+  }
+
+  excluir(id: number | undefined): void {
+    if (id !== undefined && confirm('Tem certeza que deseja excluir este colaborador?')) {
+      this.colaboradorService.excluir(id).subscribe(() => {
+        this.carregarColaboradores();
+      });
+    }
+  }
 }
