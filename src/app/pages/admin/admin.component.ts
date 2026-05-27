c77a31c9e811cbb9ffd28f5b302427eedf1bbb1b
Vinicius Charles <viniciuscharles3101@gmail.com>
2026-05-27 10:39:46 -0300

    feat: integra formulario ao componente de admin completando o crud no frontend

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ColaboradorService } from '../../services/colaborador.service';
 import { Colaborador } from '../../models/colaborador.model';
+import { ColaboradorFormComponent } from '../../components/colaborador-form/colaborador-form.component';
 
 @Component({
   selector: 'app-admin',
@@ -11,7 +13,10 @@ export class AdminComponent implements OnInit {
   colaboradores: Colaborador[] = [];
   colunasExibidas: string[] = ['id', 'nome', 'cargo', 'departamento', 'acoes'];
 
-  constructor(private colaboradorService: ColaboradorService) { }
+  constructor(
+    private colaboradorService: ColaboradorService,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
     this.carregarColaboradores();
@@ -19,7 +24,24 @@ export class AdminComponent implements OnInit {
 
   carregarColaboradores(): void {
     this.colaboradorService.listar().subscribe(dados => {
-      this.colaboradores = dados;
+      this.colaboradores = [...dados];
+    });
+  }
+
+  abrirFormulario(colaborador?: Colaborador): void {
+    const dialogRef = this.dialog.open(ColaboradorFormComponent, {
+      width: '400px',
+      data: colaborador ? { ...colaborador } : null
+    });
+
+    dialogRef.afterClosed().subscribe(resultado => {
+      if (resultado) {
+        if (resultado.id) {
+          this.colaboradorService.atualizar(resultado).subscribe(() => this.carregarColaboradores());
+        } else {
+          this.colaboradorService.adicionar(resultado).subscribe(() => this.carregarColaboradores());
+        }
+      }
     });
   }
 
