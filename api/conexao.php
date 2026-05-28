3b5b5db7a88aa553075f38bcbbf9c1a75cb9379c
Vinicius Charles <viniciuscharles3101@gmail.com>
2026-05-28 12:30:18 -0300

    feat: configura conexao padrao com o banco e headers de cors

---
+++ api/conexao.php
@@ -0,0 +1,23 @@
+<?php 
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+    if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+        exit(0);
+    }
+
+    $host = "localhost";
+    $port = "3307";
+    $db_name = "agencia_db";
+    $user = "root";
+    $pass = "";
+
+    try {
+        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db_name", $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e){
+        echo json_encode(["erro" => "Erro de conexão: " . $e->getMessage()]);
+        exit;
+    }
+ ?>
